93ecc5c74881cb6dda205484dd64b98ee53cb3ae
stxvenbillones-afk <stxvenbillones@gmail.com>
2026-09-15 16:33:39 -0400

    Actualizar socket.ts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,16 +1,30 @@
 import makeWASocket, {
-  proto,
+  Browsers,
   type AnyMessageContent,
   type SocketConfig,
   type WASocket,
-  isJidGroup
 } from "baileys";
 
 interface Socket extends WASocket {
-    sendMessage: (jid:string, content:AnyMessageContent, options?:any) => Promise<any>
+  sendMessage: (
+    jid: string,
+    content: AnyMessageContent,
+    options?: any
+  ) => Promise<any>;
 }
 
-export default async function createSocket(config: SocketConfig): Promise<WASocket> {
-      const sock: WASocket = makeWASocket(config);
-      return sock;
+export default async function createSocket(
+  config: SocketConfig
+): Promise<WASocket> {
+  const socketConfig: SocketConfig = {
+    ...config,
+
+    // Navegador oficial/canónico para evitar
+    // problemas con el código de vinculación.
+    browser: Browsers.macOS("Desktop"),
+  };
+
+  const sock: WASocket = makeWASocket(socketConfig);
+
+  return sock;
 }
