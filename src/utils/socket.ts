a2bcb537c1c10b4e9bc106e774893d1bd21d404b
stxvenbillones-afk <stxvenbillones@gmail.com>
2026-09-15 17:21:27 -0400

    Actualizar socket.ts

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -14,7 +14,7 @@ export default async function createSocket(
     `WhatsApp Web: ${version.join(".")} | latest: ${isLatest}`
   );
 
-  return makeWASocket({
+  const sock = makeWASocket({
     ...config,
     version,
     browser: Browsers.macOS("Desktop"),
@@ -22,4 +22,15 @@ export default async function createSocket(
     markOnlineOnConnect: false,
     syncFullHistory: false,
   });
+
+  // Código de vinculación
+  if (!sock.authState.creds.registered) {
+    const phoneNumber = "18295862297";
+
+    const code = await sock.requestPairingCode(phoneNumber);
+
+    console.log("🔑 Código de vinculación:", code);
+  }
+
+  return sock;
 }
