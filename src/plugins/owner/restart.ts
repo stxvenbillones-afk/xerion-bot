ec42ea8dffc0c237128e412628e3d910f9e7fa28
stxvenbillones-afk <stxvenbillones@gmail.com>
2026-09-15 20:39:42 -0400

    Actualizar restart.ts

--- src/plugins/owner/restart.ts
+++ src/plugins/owner/restart.ts
@@ -2,16 +2,26 @@ import cmd, { type CommandContext } from "../../commands/map.js";
 
 cmd.add({
   name: "restart",
-  alias: ["reboot"],
+  alias: ["reiniciar"],
   category: ["owner"],
   desc: "Reinicia XERION BOT.",
   isOwner: true,
 
   async run({ m }: CommandContext) {
-    await m.reply("🔄 *XERION BOT* se está reiniciando...");
+    await m.reply(
+      `╭━━━━━━━━━━━━━━━━━━╮
+┃ 🔄 𝗥𝗘𝗜𝗡𝗜𝗖𝗜𝗔𝗡𝗗𝗢 𝗫𝗘𝗥𝗜𝗢𝗡
+╰━━━━━━━━━━━━━━━━━━╯
+
+┃ ⏳ Reiniciando el bot...
+┃ 👻 Volveré a estar online en unos segundos.
+
+╰━━━━━━━━━━━━━━━━━━╯
+      𝗫𝗘𝗥𝗜𝗢𝗡 𝗕𝗢𝗧`
+    );
 
     setTimeout(() => {
       process.exit(0);
-    }, 1000);
+    }, 1500);
   },
 });
