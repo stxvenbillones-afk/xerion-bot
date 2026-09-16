1db34da59a751e0a77f4ed77f9c7c17772bc63bf
stxvenbillones-afk <stxvenbillones@gmail.com>
2026-09-15 20:40:13 -0400

    Actualizar shutdown.ts

--- src/plugins/owner/shutdown.ts
+++ src/plugins/owner/shutdown.ts
@@ -2,16 +2,26 @@ import cmd, { type CommandContext } from "../../commands/map.js";
 
 cmd.add({
   name: "shutdown",
-  alias: ["off", "stopbot"],
+  alias: ["apagar", "stopbot"],
   category: ["owner"],
-  desc: "Apaga XERION BOT por completo.",
+  desc: "Apaga XERION BOT.",
   isOwner: true,
 
   async run({ m }: CommandContext) {
-    await m.reply("🛑 *XERION BOT* se está apagando...");
+    await m.reply(
+      `╭━━━━━━━━━━━━━━━━━━╮
+┃ 🔴 𝗫𝗘𝗥𝗜𝗢𝗡 𝗕𝗢𝗧
+╰━━━━━━━━━━━━━━━━━━╯
+
+┃ 🛑 Apagando el bot...
+┃ 👻 XERION BOT está offline.
+
+╰━━━━━━━━━━━━━━━━━━╯
+      𝗫𝗘𝗥𝗜𝗢𝗡 𝗕𝗢𝗧`
+    );
 
     setTimeout(() => {
       process.exit(0);
-    }, 1000);
+    }, 1500);
   },
 });
